ab055ce8744ad07e36032e1909cb99596efcd5c4
Andrii Dudko <Annbpiu@gmail.com>
2024-09-14 11:38:23 +0300

    make build

---
+++ index.aeb96f3a.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("tr"),r=[];e.forEach(function(e,t){r.push(e.cells[1].outerHTML);var c=r[t];e.cells[3].insertAdjacentHTML("afterend",c)});
+//# sourceMappingURL=index.aeb96f3a.js.map
